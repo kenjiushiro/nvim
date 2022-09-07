9bbba1215ed615b26fbde8dfc58140e6f3b7a559
Kenji <kenjimartinushiro@gmail.com>
2022-09-07 11:05:17 -0300

    Add php dap

--- lua/modules/plugins/dap/php.lua
+++ lua/modules/plugins/dap/php.lua
@@ -1,4 +1,7 @@
 local dap = require('dap')
+-- Install php debugger from https://github.com/xdebug/vscode-php-debug
+-- https://xdebug.org/wizard
+
 dap.adapters.php = {
     type = 'executable',
     command = 'node',
@@ -8,23 +11,44 @@ dap.adapters.php = {
 }
 
 dap.configurations.php = {
-    {
-        type = 'php',
-        request = 'launch',
-        name = 'Launch',
-        port = '9003',
-        log = true,
-        serverSourceRoot = '/srv/www/',
-        localSourceRoot = '/home/www/VVV/www/',
-    },
-    {
-        type = 'php',
-        request = 'attach',
-        name = 'Attach',
-        port = '9003',
-        cwd = vim.fn.getcwd(),
-        log = true,
-        serverSourceRoot = '/srv/www/',
-        localSourceRoot = '/home/www/VVV/www/',
-    },
+  {
+      name = "Listen for Xdebug",
+      type = "php",
+      request = "launch",
+      port = 9003
+  },
+  {
+      name = "Launch currently open script",
+      type = "php",
+      request = "launch",
+      program = "${file}",
+      cwd = "${fileDirname}",
+      port = 0,
+      runtimeArgs = {
+          "-dxdebug.start_with_request=yes"
+        },
+      env = {
+          XDEBUG_MODE = "debug,develop",
+          XDEBUG_CONFIG = "client_port=${port}"
+      }
+  },
+  {
+      name = "Launch Built-in web server",
+      type = "php",
+      request = "launch",
+      runtimeArgs = {
+          "-dxdebug.mode=debug",
+          "-dxdebug.start_with_request=yes",
+          "-S",
+          "localhost:0"
+        },
+      program = "",
+      cwd = "${workspaceRoot}",
+      port = 9003,
+      serverReadyAction = {
+          pattern = "Development Server \\(http://localhost:([0-9]+)\\) started",
+          uriFormat = "http://localhost:%s",
+          action = "openExternally"
+      }
+  }
 }
