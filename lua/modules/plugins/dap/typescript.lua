ff2ef2a077111421641f56200ea4756da21c1cbe
Kenji <kenjimartinushiro@gmail.com>
2022-09-27 18:19:11 -0300

    WIP

--- lua/modules/plugins/dap/typescript.lua
+++ lua/modules/plugins/dap/typescript.lua
@@ -17,76 +17,85 @@ local dap = require('dap')
 --       })
 -- end
 
-dap.adapters.node2 = {
-    type = "executable",
-    command = "node",
-    args = {
-        os.getenv("HOME") .. "/.config/debuggers/vscode-node-debug2/out/src/nodeDebug.js",
-    },
-}
+local mason_registry = require'mason-registry'
+local bin_path = vim.fn.stdpath('data') .. '/mason/bin/'
+
+local node_debugger = "node-debug2-adapter"
+  if (mason_registry.is_installed(node_debugger)) then
+    local adapter_path = bin_path .. node_debugger
+    print('Adapter: ' .. adapter_path)
+
+    dap.adapters.node2 = {
+        type = "executable",
+        command = "node",
+        args = {
+          adapter_path
+        },
+    }
+
+  local port = 9229
+  dap.configurations.typescript = {
+      {
+        type = 'node2',
+        request = 'attach',
+        name = "Attach",
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        protocol = 'inspector',
+        skipFiles = {'<node_internals>/**/*.js'},
+        port = function()
+          port = tonumber(vim.fn.input('Port [9229]: ')) or 9229
+          print('Debugging port '.. port)
+          return port
+        end
+      },
+      {
+        type = "node2",
+        request = "launch",
+        stopOnEntry = true,
+        name = "Launch Program",
+        skipFiles = {
+            "<node_internals>/**"
+        },
+        program = "${file}",
+        sourceMaps = true,
+        outFiles = {
+            "${workspaceFolder}/dist/*.js",
+            "${workspaceFolder}/bin/*.js",
+            "${workspaceFolder}/out/*.js",
+            "${workspaceFolder}/dist/**/*.js",
+            "${workspaceFolder}/bin/**/*.js",
+            "${workspaceFolder}/out/**/*.js",
+        },
+      },
+      {
+          name = "Attach to Chrome",
+          port = 9222,
+          request = "attach",
+          type = "chrome",
+          webRoot = "${workspaceFolder}",
+      },
+  }
+
+  dap.configurations.javascript = {
+      {
+        type = "node2",
+        request = "launch",
+        stopOnEntry = true,
+        name = "Launch Program",
+        skipFiles = {
+            "<node_internals>/**"
+        },
+        program = "${file}"
+      },
+  }
+end
+
 
 dap.adapters.chrome = {
     type = "executable",
     command = "node",
     args = {
         os.getenv("HOME") .. "/.config/debuggers/vscode-chrome-debug/out/src/chromeDebug.js",
-    },
-}
-
-local port = 9229
-
-dap.configurations.typescript = {
-    {
-      type = 'node2',
-      request = 'attach',
-      name = "Attach",
-      cwd = vim.fn.getcwd(),
-      sourceMaps = true,
-      protocol = 'inspector',
-      skipFiles = {'<node_internals>/**/*.js'},
-      port = function()
-        port = tonumber(vim.fn.input('Port [9229]: ')) or 9229
-        print('Debugging port '.. port)
-        return port
-      end
-    },
-    {
-      type = "node2",
-      request = "launch",
-      stopOnEntry = true,
-      name = "Launch Program",
-      skipFiles = {
-          "<node_internals>/**"
-      },
-      program = "${file}",
-      sourceMaps = true,
-      outFiles = {
-          "${workspaceFolder}/dist/*.js",
-          "${workspaceFolder}/bin/*.js",
-          "${workspaceFolder}/out/*.js",
-          "${workspaceFolder}/dist/**/*.js",
-          "${workspaceFolder}/bin/**/*.js",
-          "${workspaceFolder}/out/**/*.js",
-      },
-    },
-    {
-        name = "Attach to Chrome",
-        port = 9222,
-        request = "attach",
-        type = "chrome",
-        webRoot = "${workspaceFolder}",
-    },
-}
-
-dap.configurations.javascript = {
-    {
-      type = "node2",
-      request = "launch",
-      stopOnEntry = true,
-      name = "Launch Program",
-      skipFiles = {
-          "<node_internals>/**"
-      },
-      program = "${file}"
     },
 }
