36a2c921232f480971e7ab9c6c68afb14e20636b
Kenji Ushiro <kenjiushiro@pop-os.localdomain>
2022-09-28 00:12:54 -0300

    Fix stopped sign

--- lua/modules/plugins/dap.lua
+++ lua/modules/plugins/dap.lua
@@ -1,7 +1,7 @@
 local dap = require('dap')
 
 vim.fn.sign_define('DapBreakpoint', {text='🔴', texthl='', linehl='', numhl=''})
-vim.fn.sign_define('DapStopped', {text='➡️', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapStopped', {text='🔷', texthl='', linehl='', numhl=''})
 vim.fn.sign_define('DapLogPoint', {text='📝', texthl='', linehl='', numhl=''})
 vim.fn.sign_define('DapBreakpointCondition', {text='👀', texthl='', linehl='', numhl=''})
 vim.fn.sign_define('DapBreakpointRejected', {text='🚨', texthl='', linehl='', numhl=''})
