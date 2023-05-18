cd84890be50febfca9cf04ffe3e995d7304d0973
Kenji Ushiro <kenjimartinushiro@gmail.com>
2023-05-18 14:53:53 -0300

    Make initial changes for powershell compatibility

---
+++ lua/modules/plugins/dap.lua
@@ -0,0 +1,23 @@
+-- return {}
+-- local dap = require('dap')
+
+-- vim.fn.sign_define('DapBreakpoint', {text='🔴', texthl='', linehl='', numhl=''})
+-- vim.fn.sign_define('DapStopped', {text='🔷', texthl='', linehl='', numhl=''})
+-- vim.fn.sign_define('DapLogPoint', {text='📝', texthl='', linehl='', numhl=''})
+-- vim.fn.sign_define('DapBreakpointCondition', {text='👀', texthl='', linehl='', numhl=''})
+-- vim.fn.sign_define('DapBreakpointRejected', {text='🚨', texthl='', linehl='', numhl=''})
+
+-- local bin_path = vim.fn.stdpath('data') .. '/mason/bin/'
+
+-- return {
+--     setup = function()
+--         require'dapui'.setup()
+--         require("dap-python").setup('~/.virtualenvs/debugpy/bin/python')
+--         require("dap-go").setup()
+--         require("nvim-dap-virtual-text").setup()
+--         require'modules.plugins.dap.php'.setup(bin_path)
+--         require'modules.plugins.dap.typescript'.setup(bin_path)
+--         require'modules.plugins.dap.csharp'.setup(bin_path)
+--         require'modules.plugins.dap.bash'.setup(bin_path)
+--     end
+-- }
