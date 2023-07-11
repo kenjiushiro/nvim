832429e15d94e531652cfbf9b31059f704b5cb7b
Kenji Ushiro <kenjiushiro@pop-os.localdomain>
2023-07-11 18:51:30 -0300

    Add mason-nvim-dap

--- lua/modules/plugins/formatter.lua
+++ lua/modules/plugins/formatter.lua
@@ -7,25 +7,25 @@ local function fmt(args)
 end
 
 M.prettier = function(opts)
-    return fmt {
+    return fmt({
         exe = "prettier",
-        args = vim.tbl_flatten { "--stdin-filepath", vim.api.nvim_buf_get_name(0), opts or {} },
+        args = vim.tbl_flatten({ "--stdin-filepath", vim.api.nvim_buf_get_name(0), opts or {} }),
         stdin = true,
-    }
+    })
 end
 
 -- npm install -g @fsouza/prettierd
 -- run `prettierd start`
-M.prettierd = fmt {
+M.prettierd = fmt({
     exe = "prettierd",
     args = { vim.api.nvim_buf_get_name(0) },
     stdin = true,
-}
+})
 
-M.clang = fmt {
+M.clang = fmt({
     exe = "clang-format",
     args = {
-        "-assume-filename=" .. vim.fn.expand "%:t",
+        "-assume-filename=" .. vim.fn.expand("%:t"),
         [[
         -style='{UseTab: Never,
         IndentWidth: 4,
@@ -39,51 +39,47 @@ M.clang = fmt {
         ]],
     },
     stdin = true,
-}
+})
 
-M.stylua = fmt {
+M.stylua = fmt({
     exe = "stylua",
     args = { "--search-parent-directories", "--stdin-filepath", '"%:p"', "--", "-" },
     stdin = true,
-}
+})
 
-M.sh = fmt {
+M.sh = fmt({
     exe = "shfmt",
     args = { "-i " .. vim.opt.shiftwidth:get() },
     stdin = true,
-}
+})
 
-M.black = fmt {
+M.black = fmt({
     exe = "black",
     args = { "--quiet", "-" },
     stdin = true,
-}
+})
 
-M.goimports = fmt {
+M.goimports = fmt({
     exe = "goimports",
     stdin = true,
-}
+})
 
-M.rustfmt = fmt {
+M.rustfmt = fmt({
     exe = "rustfmt",
     args = {
-        string.format(
-            "--config hard_tabs=%s,tab_spaces=%s",
-            not vim.opt.expandtab:get(),
-            vim.opt.shiftwidth:get()
-        ),
+        string.format("--config hard_tabs=%s,tab_spaces=%s", not vim.opt.expandtab:get(), vim.opt.shiftwidth:get()),
     },
     stdin = true,
-}
+})
 
-M.latex = fmt {
+M.latex = fmt({
     exe = "latexindent",
     args = { "-g /dev/stderr", "2>/dev/null" },
     stdin = true,
-}
+})
 
 M.config = function()
-    require("formatter").setup {
+    require("formatter").setup({
         logging = false,
         filetype = {
             sh = { M.sh },
@@ -96,18 +92,18 @@ M.config = function()
             python = { M.black },
             go = { M.goimports },
             rust = { M.rustfmt },
-            css = { M.prettier "--parser css" },
+            css = { M.prettier("--parser css") },
             xml = { M.prettier() },
             json = { M.prettier() },
-            html = { M.prettier "--parser html" },
+            html = { M.prettier("--parser html") },
             scss = { M.prettier() },
             markdown = { M.prettier() },
-            javascript = { M.prettier { "--single-quote" } },
-            typescript = { M.prettier { "--single-quote" } },
-            javascriptreact = { M.prettier { "--single-quote" } },
-            typescriptreact = { M.prettier { "--single-quote" } },
+            javascript = { M.prettier({ "--single-quote" }) },
+            typescript = { M.prettier({ "--single-quote" }) },
+            javascriptreact = { M.prettier({ "--single-quote" }) },
+            typescriptreact = { M.prettier({ "--single-quote" }) },
         },
-    }
+    })
 end
 
 return M
