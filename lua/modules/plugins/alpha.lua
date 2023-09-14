a6659b051bdd5c849933ac69b720aadf1509797b
Kenji Ushiro <kenjimartinushiro@gmail.com>
2023-09-14 10:01:56 -0300

    Remove unused shortcuts from Dashboard

--- lua/modules/plugins/alpha.lua
+++ lua/modules/plugins/alpha.lua
@@ -7,12 +7,9 @@ M.config = function()
     math.randomseed(os.time())
     dashboard.section.header.val = banners[math.random(#banners)]
     dashboard.section.buttons.val = {
-      dashboard.button("e", "> 📄 New file", ":enew<CR>"),
       dashboard.button("f", "> 🔎 Find files", ":Telescope fd<CR>"),
-      dashboard.button("r", "> 📞 HTTP Client", ":Telescope fd cwd=$HOME/requests/<CR>"),
       dashboard.button("p", "> 💼 Select project", ":Telescope projects<CR>"),
       dashboard.button("n", "> 📝 Nvim Config", ":lua require('telescope.builtin').find_files({ cwd = vim.fn.stdpath('config') })<CR>"),
-      dashboard.button("a", "> 💻 Alacritty config", ":tabe $HOME/.config/alacritty/alacritty.yml<CR>"),
       dashboard.button("s", "> 🔌 Sync plugins", ":PackerSync<CR>"),
       dashboard.button("g", "> 🌿 Git", ":Git<CR>"),
       dashboard.button("q", "> 👋 Quit", ":qa!<CR>"),
