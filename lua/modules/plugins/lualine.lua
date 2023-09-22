2b8d2828a581b0315bf747ac409355f75851446f
Kenji Martin Ushiro <kenjimartinushiro@gmail.com>
2023-09-22 11:07:01 -0300

    Change tabby and lualine theme to bubbly

--- lua/modules/plugins/lualine.lua
+++ lua/modules/plugins/lualine.lua
@@ -1,6 +1,39 @@
 local M = {}
 M.config = function()
     local lualine = require("lualine")
-    lualine.setup({})
+    local colors = {
+        blue = "#5E81AC",
+        cyan = "#88C0D0",
+        black = "#2E3440",
+        white = "#ECEFF4",
+        red = "#BF616A",
+        violet = "#B48EAD",
+        grey = "#4C566A",
+        green = "#A3BE8C",
+        orange = "#D08770",
+    }
+    lualine.setup({
+        options = {
+            theme = {
+                normal = {
+                    a = { fg = colors.black, bg = colors.blue },
+                    b = { fg = colors.white, bg = colors.grey },
+                    c = { fg = colors.gery, bg = colors.black },
+                },
+
+                insert = { a = { fg = colors.black, bg = colors.green } },
+                visual = { a = { fg = colors.black, bg = colors.violet } },
+                replace = { a = { fg = colors.black, bg = colors.red } },
+
+                inactive = {
+                    a = { fg = colors.white, bg = colors.black },
+                    b = { fg = colors.white, bg = colors.black },
+                    c = { fg = colors.black, bg = colors.black },
+                },
+            },
+            component_separators = "|",
+            section_separators = { left = "", right = "" },
+        },
+    })
 end
 return M
--- lua/modules/plugins/tabby.lua
+++ lua/modules/plugins/tabby.lua
@@ -11,17 +11,17 @@ require("tabby.tabline").set(function(line)
     return {
         {
             { "  ", hl = theme.head },
-            line.sep("", theme.head, theme.fill),
+            line.sep("", theme.head, theme.fill),
         },
         line.tabs().foreach(function(tab)
             local hl = tab.is_current() and theme.current_tab or theme.tab
             return {
-                line.sep("", hl, theme.fill),
+                line.sep(" ", hl, theme.fill),
                 tab.is_current() and "" or "",
-                tab.number(),
+                tab.current_win().file_icon(),
                 tab.name(),
                 tab.close_btn(""),
-                line.sep("", hl, theme.fill),
+                line.sep("", hl, theme.fill),
                 hl = hl,
                 margin = " ",
             }
@@ -29,16 +29,17 @@ require("tabby.tabline").set(function(line)
         line.spacer(),
         line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
             return {
-                line.sep("", theme.win, theme.fill),
+                line.sep(" ", theme.win, theme.fill),
                 win.is_current() and "" or "",
+                win.file_icon(),
                 win.buf_name(),
-                line.sep("", theme.win, theme.fill),
+                line.sep("", theme.win, theme.fill),
                 hl = theme.win,
                 margin = " ",
             }
         end),
         {
-            line.sep("", theme.tail, theme.fill),
+            line.sep(" ", theme.tail, theme.fill),
             { "  ", hl = theme.tail },
         },
         hl = theme.fill,
