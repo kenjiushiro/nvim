9fea1c81d4f665b29aced9070c8f6a9d7bcccaf9
Kenji Martin Ushiro <kenjimartinushiro@gmail.com>
2024-09-23 09:39:35 -0300

    Add supermaven

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,6 +6,7 @@ return {
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
+      table.insert(opts.sources, { name = "supermaven" })
     end,
   },
 }
