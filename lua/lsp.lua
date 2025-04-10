2d3e75a6cbee2247beb541f41f4ea0f0fada5e01
Kenji Ushiro <kenji.m.ushiro@gsk.com>
2025-04-10 09:12:07 -0300

    Improve lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(event)
     local opts = { buffer = event.buf }
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
     vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
@@ -12,12 +12,24 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "<C-p>", vim.diagnostic.goto_prev)
     vim.keymap.set("n", "<C-n>", vim.diagnostic.goto_next)
     vim.keymap.set("n", "<leader>ll", vim.diagnostic.open_float)
-    vim.keymap.set("n", "<leader>lll", vim.diagnostic.setloclist)
+    vim.keymap.set("n", "<leader>LL", vim.diagnostic.setloclist)
 
     vim.keymap.set("n", "<leader>lS", vim.lsp.buf.workspace_symbol )
     vim.keymap.set("n", "<leader>ls", vim.lsp.buf.document_symbol )
 
     vim.keymap.set("n", "<leader>fo", function() vim.lsp.buf.format { async = true } end)
+
+    local signs = {
+      Error = "✘",       -- or "❌" or "E"
+      Warn  = "▲",       -- or "⚠️"
+      Hint  = "💡",       -- or "H"
+      Info  = "ℹ️",       -- or "I"
+    }
+
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
 
   end,
 })
