752af649d963363d75fcc3f44e0556fd15f2e390
kenjiushiro <kenjiushiro@Mac.fibertel.com.ar>
2026-02-01 21:25:33 -0300

    Widen chat

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,6 +1,15 @@
 return {
   "yetone/avante.nvim",
   keys = {
-    { "<leader>/", mode = { "n", "i" }, "<cmd>AvanteChat<cr>", desc = "Open chat window" },
+    {
+          "<leader>/",
+          mode = { "n", "i" },
+          function()
+            local win_width = vim.o.columns
+            vim.cmd("AvanteChat")
+            vim.cmd("vertical resize " .. math.floor(win_width / 2))
+          end,
+          desc = "Open chat window occupying half the screen",
+        },
   },
 }
